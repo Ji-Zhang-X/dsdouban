3991c0575f4831af90ae2a06c7de1eff6abc6216
leapeto <313305185@qq.com>
2022-05-04 23:41:42 +0800

    为几个html添加了导航栏

--- DSDouBan/settings.py
+++ DSDouBan/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'DSDouBan.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'UserSystem/' ,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
