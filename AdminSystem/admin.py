16ab9a1a9114b16ede1d9ec7a412c5a4d75be6c0
leapeto <313305185@qq.com>
2022-06-02 21:42:27 +0800

    让管理员和超级管理员可以正常删除用户

--- AdminSystem/admin.py
+++ AdminSystem/admin.py
@@ -3,6 +3,7 @@ from django import forms
 # Register your models here.
 from AdminSystem.models import Admin,User
 from .utils.encrypt import md5
+from . import models
 
 class MyUserForm(forms.ModelForm):
     def clean_password(self):
@@ -23,7 +24,22 @@ class Usermodel(admin.ModelAdmin):
     list_per_page = 10
     form = MyUserForm
 
+    def delete_model(self, request, obj):
+        nid = obj.user_id
+        orders = models.Order.objects.filter(user_id=nid)
+        for oneorder in orders:
+            models.OrderList.objects.filter(order_id=oneorder.order_id).delete()
+        orders.delete()
+        obj.delete()
 
+    def delete_queryset(self,request, queryset):
+        for user in queryset:
+            nid = user.user_id
+            orders = models.Order.objects.filter(user_id=nid)
+            for oneorder in orders:
+                models.OrderList.objects.filter(order_id=oneorder.order_id).delete()
+            orders.delete()
+            user.delete()
 
 
 admin.site.register(Admin, Adminmodel)
